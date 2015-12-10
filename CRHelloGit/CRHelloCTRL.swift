572b4db4be74014287b94f368db8140319a23342
Cáceres <carpe_diem@mac.com>
2015-12-10 19:02:22 -0400

    Se añadió el archivo CRHelloCTRL

---
+++ CRHelloGit/CRHelloCTRL.swift
@@ -0,0 +1,35 @@
+//
+//  CRHelloCTRL.swift
+//  CRHelloGit
+//
+//  Created by Cáceres on 12/10/15.
+//  Copyright © 2015 Firichi. All rights reserved.
+//
+
+import UIKit
+
+class CRHelloCTRL: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
